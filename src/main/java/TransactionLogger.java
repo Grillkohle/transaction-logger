43c9178914503da2d8dc590124b552f0183a8be0
Frieder Ulm <frieder.ulm@zalando.de>
2017-07-31 12:11:03 +0200

    added some dependencies, default configuration file, application class

---
+++ src/main/java/TransactionLogger.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionLogger { 
+    
+    public static void main(final String [] args){
+        SpringApplication.run(this, args);
+    }
+}
