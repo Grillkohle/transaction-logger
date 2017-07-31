ac5d754ab82d7ed2f46fe5b8b744fbce6ee1d8e6
Frieder Ulm <frieder.ulm@zalando.de>
2017-07-31 12:44:39 +0200

    added start class, transaction DTO

--- src/main/java/TransactionLogger.java
+++ src/main/java/TransactionLogger.java
@@ -2,9 +2,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TransactionLogger { 
-    
-    public static void main(final String [] args){
-        SpringApplication.run(this, args);
+public class TransactionLogger {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(TransactionLogger.class, args);
     }
 }
---
+++ src/main/java/web/dto/TransactionRestDTO.java
@@ -0,0 +1,10 @@
+package web.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Builder
+@Data
+@ToString
+public class TransactionRestDTO { }
